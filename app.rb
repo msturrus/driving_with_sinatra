50d44d219863c96a6a6a2e61e3efc7c772510322
Michael Sturrus <msturrus@gmail.com>
2016-03-17 13:02:09 -0500

    done

--- app.rb
+++ app.rb
@@ -1,2 +1,29 @@
 require 'bundler'
 Bundler.require()
+
+get '/' do
+  scott = {
+    :height => 81,
+    :computer => "macbook pro"
+  }.to_json
+
+  mike = {
+    :height => 74,
+    :computer => "macbook pro"
+  }.to_json
+
+end
+
+get '/mike' do
+  mike = {
+    :cheeseburger => "stolen",
+    :huger_level => 0
+  }.to_json
+end
+
+get '/scott' do
+  scott = {
+    :cheeseburger => nil,
+    :huger_level => 100000
+  }.to_json
+end
